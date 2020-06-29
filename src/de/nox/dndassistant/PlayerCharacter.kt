d68b64c869373a61195bcdb6225097135a47a822
Nox Le <noxsense@gmail.com>
2020-06-29 12:29:06 +0200

    Interface for Skills and Tools with Proficiency
    - Add an interface for anything, proficiency can be added.
    - the normal Skill (athletics, arcana, etc.) derives as Skill
    - Artisan Tools or other kits derives as Tool or Toolskills
    - Weapon Proficiency derives as Weapon or WeaponSkill.

--- src/de/nox/dndassistant/PlayerCharacter.kt
+++ src/de/nox/dndassistant/PlayerCharacter.kt
@@ -138,36 +138,3 @@ enum class Ability(val fullname : String) {
 	WIS("WISDOM"),
 	CHA("CHARISMA")
 }
-
-/* Body skill.*/
-enum class Skill(val source: Ability) {
-	ATHLETICS(Ability.STR),
-
-	ACROBATICS(Ability.DEX),
-	SLEIGHT_OF_HAND(Ability.DEX),
-	STEALTH(Ability.DEX),
-
-	ARCANA(Ability.INT),
-	HISTORY(Ability.INT),
-	INVESTIGATION(Ability.INT),
-	NATURE(Ability.INT),
-	RELIGION(Ability.INT),
-
-	ANIMAL(Ability.WIS),
-	INSIGHT(Ability.WIS),
-	MEDICINE(Ability.WIS),
-	PERCEPTION(Ability.WIS),
-	SURVIVAL(Ability.WIS),
-
-	DECEPTION(Ability.CHA),
-	INTIMIDATION(Ability.CHA),
-	PERFORMANCE(Ability.CHA),
-	PERSUASION(Ability.CHA)
-}
-
-/* Proficiency level.*/
-enum class Proficiency(val factor: Int){
-	NONE(0),
-	PROFICIENT(1),
-	EXPERT(2)
-}
