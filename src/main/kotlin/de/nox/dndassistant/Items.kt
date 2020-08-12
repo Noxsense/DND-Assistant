c28c98f3d819e3e626bf619d02b7c113774ef545
Nox Le <noxsense@gmail.com>
2020-08-12 23:00:34 +0200

    Add Container functions to access inside items

--- src/main/kotlin/de/nox/dndassistant/Items.kt
+++ src/main/kotlin/de/nox/dndassistant/Items.kt
@@ -66,24 +66,89 @@ data class Container(
 		= ((if (thisInclusive) weight else 0.0 )
 		+ inside.sumByDouble { if (it is Container) it.sumWeight(true) else it.weight })
 
+	/** Check if this container contains a specific item, by reference. */
+	operator fun contains(item: Item) : Boolean
+		= inside.filter { it === item }.size > 0
+
+	/** Check if this item contains an item with the matching name. */
+	operator fun contains(itemName: String) : Boolean
+		= inside.filter { it.name == itemName }.size > 0
+
+	/** Check if the Container contains all requested items, by references. */
+	fun containsAll(items: Collection<Item>): Boolean
+		= items.all { contains(it) }
+	
+	/** Check if the Container contains all requested items names. */
+	fun containsAllNames(itemNames: Collection<String>) : Boolean
+		= itemNames.all { contains(it) }
+
 	/** Add a new item to the bag. */
 	fun insert(item: Item) {
-		inside += item
+		if (item !in this) inside += item
 	}
+
+	fun filter(filter: (item: Item) -> Boolean) : List<Item>
+		= inside.filter(filter)
+
+	fun indexOf(item: Item) : Int
+		= inside.withIndex().filter { it === item }.run {
+			if (isEmpty()) -1 else get(0).index
+		}
+
+	fun insideOf(itemName: String) : Int
+		= inside.map { it.name }.indexOf(itemName)
+
+	fun indexOfGroupedItem(groupName: String, indexInGroup: Int) : Int {
+		return inside.withIndex()
+			.filter { it.value.name == groupName }
+			.get(indexInGroup).index
+	}
+
+	fun clear() : List<Item> {
+		val dropped = inside
+		inside = listOf()
+		return dropped
+	}
+
+	/** Remove all items, that matches the filter.
+	 * @return dropped items. */
+	fun removeAll(predicate: (index: Int, item: Item) -> Boolean): List<Item> {
+		val partition = inside.withIndex().partition { predicate(it.index, it.value) }
+		inside = partition.second.map { it.value }
+		return partition.first.map { it.value }
+	}
+
+	/** Retain all items, that matches the filter.
+	 * @return dropped items. */
+	fun retainAll(filter: (index: Int, item: Item) -> Boolean): List<Item>
+		= removeAll { i, x -> !filter(i, x) } // drop non matching items.
 
 	/** Remove an item from the bag.
 	 * @param item item to remove. */
-	fun remove(item: Item) {
-		inside -= item
+	fun remove(item: Item) : Item? {
+		return removeAll { _, it -> it === item }[0]
+	}
+
+	/** Remove the first item, that matches the name. */
+	fun remove(itemName: String) : Item? {
+		val item : Item?
+			= inside.filter { it.name == itemName }.getOrElse(0, { null })
+
+		if (item != null) {
+			remove(item)
+		}
+
+		return item
 	}
 
 	/** Remove an item from the bag.
 	 * @param index index of the item, which will be removed. */
-	fun removeAt(index: Int) : Item? {
+	fun remove(index: Int) : Item? {
 		if (index in (0 until inside.size)) {
 			val item = inside[index]
 
-			inside = inside.subList(0, index) + inside.subList(index + 1, inside.size)
+			// remove all with matching index.
+			inside = removeAll { i, _ -> i == index }
 
 			return item
 
