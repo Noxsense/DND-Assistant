d7cd38c7cf1c843bfa1c38ae77f301a7bfb2cd51
Nox Le <noxsense@gmail.com>
2020-09-03 19:52:23 +0200

    New structure for gradle
    Restructure gradle direcotories

--- sourceCode/settings.gradle.kts
+++ sourceCode/settings.gradle.kts
@@ -4,7 +4,9 @@
  * The settings file is used to specify which projects to include in your build.
  *
  * Detailed information about configuring a multi-project build in Gradle can be found
- * in the user manual at https://docs.gradle.org/6.5/userguide/multi_project_builds.html
+ * in the user manual at https://docs.gradle.org/6.6/userguide/multi_project_builds.html
  */
 
-rootProject.name = "DnD-Assistant"
+rootProject.name = "DnD-Assitant"
+
+include("core")
