c6e53f856dbac2fb5d161af26813645a33d4c0e9
Nox <noxsense@gmail.com>
2024-08-01 00:16:20 +0200

    Remove Old Dice Term.

--- android-app/src/main/java/de/nox/dndassistant/app/MainActivity.kt
+++ android-app/src/main/java/de/nox/dndassistant/app/MainActivity.kt
@@ -19,8 +19,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 // import kotlinx.android.synthetic.main.activity_main.*
 
-import de.nox.dndassistant.core.D20
-
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 	private val log = LoggerFactory.getLogger("D&D Main")
@@ -101,89 +99,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 		 * Fill them with current hero's data. */
 		updateViews(true) // initiation.
 
-		/* Show rolls. */
-		// TODO (2020-10-11) proper class for Array adapter or outsources
-		label_rolls.setOnClickListener(this)
-		(content_rolls.findViewById<ListView>(R.id.list_rolls)).run {
-			adapter = object: ArrayAdapter<RollResult>(
-				this@MainActivity,
-				R.layout.list_item_roll) {
-					override fun getItem(p0: Int) : RollResult
-						= Rollers.history.toList().get(p0)
-
-					override fun getCount() : Int
-						= Rollers.history.size
-
-					override fun getView(i: Int, v: View?, parent: ViewGroup) : View {
-						if (v == null) {
-							val newView = li.inflate(R.layout.list_item_roll, parent, false)
-							return getView(i, newView, parent)
-						}
-
-						// no null
-
-						val e = getItem(i) // the element: RollResult to show
-
-						/* +-------+----------------- +
-						 * | ROLL  | rolls, why, when |
-						 * +-------+------------------+ */
-
-						(v.findViewById<TextView>(R.id.value))
-							.text = "${e.value}"
-
-						(v.findViewById<TextView>(R.id.single_rolls))
-							.text = e.single.joinToString(" + ")
-
-						(v.findViewById<TextView>(R.id.note))
-							.text = e.reason
-
-						(v.findViewById<TextView>(R.id.timestamp))
-							.text = e.timestampString
-
-						when (e.single.first()) {
-							1 -> v.setBackgroundColor(0xff0000)
-							20 -> v.setBackgroundColor(0x00ff00)
-							else -> Unit // pass
-						}
-
-						return v
-					}
-				}
-		}
-
-		/* Initiate extra rolls (from grid_dice panel). */
-		initiateExtraRolls()
-
-		panelRolls = Pair(label_rolls, content_rolls as ViewGroup)
-
-		/* Open content panels on click. */
-		// inspiration.setOnClickListener(this)
-		// label_skills.setOnClickListener(this)
-		// label_attacks.setOnClickListener(this@MainActivity)
-		// label_inventory.setOnClickListener(this)
-		// label_classes.setOnClickListener(this)
-		// label_race_background.setOnClickListener(this)
-
 		// TODO (2020-11-22) keep screen on?
 		window.addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-	}
-
-	/** Poke the roll history view to display potentially updated entries.
-	 * Also display the most recent result in the preview. */
-	public fun notifyRollsUpdated() {
-		if (!isInitializedRolls()) return
-
-		val (preview, content) = panelRolls
-		content.findViewById<ListView>(R.id.list_rolls).run {
-			(adapter as ArrayAdapter<RollResult>).notifyDataSetChanged()
-
-			/* Show last rolls, if available. */
-			preview.text = formatLabel(
-				getString(R.string.title_rolls),
-				try { "Latest: ${(adapter.getItem(0) as RollResult?)?.value}" }
-				catch (e: IndexOutOfBoundsException) { "No rolls yet." }
-				)
-		}
 	}
 
 	// TODO (2020-10-12) handle on resume, to reload views after standby?
@@ -192,8 +109,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 	/** Update the hero specific panels: fill them with hero's data. */
 	private fun updateViews(initiation: Boolean = false) {
 		log.debug("Lvl (XP) displayed.")
-
-		notifyRollsUpdated()
 
 		// XXX (2020-11-10) Proper ticker.
 		findViewById<TextView>(R.id.tick).run {
@@ -211,41 +126,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 	/** Format the preview label. */
 	private fun formatLabel(a: String, b: String) :String
 		= getString(R.string.panel_label_format).format(a, b)
-
-	/** Initiate the panel with extra dice.
-	 * There are extra dice and an additional custom field,
-	 * to roll outside of possible ability or attack rolls. */
-	private fun initiateExtraRolls() {
-		/* Assign this listener to each custom dice term. */
-		val extraDice = content_rolls.findViewById<GridLayout>(R.id.grid_dice)
-
-		log.debug("Set up extra dice to roll.")
-
-		(0 until extraDice.getChildCount()).forEach {
-			extraDice.getChildAt(it).apply {
-				if (this is EditText) {
-					/* Parse the text and return the rolled text. */
-					val editRoller = OnEventRoller.Builder(this)
-						.setReasonView(this)
-						.create()
-
-					/* Insert term and roll it. (keep text) */
-					setOnKeyListener(editRoller)
-
-					/* On Long click: Parse last term. */
-					setOnLongClickListener(editRoller)
-
-				} else if (this is TextView) {
-					/* simply roll the die */
-					setOnClickListener(OnEventRoller.Builder(this)
-						.setReasonView(this)
-						.create())
-				}
-			}
-		}
-
-		log.debug("Extra Rolls are initiated.")
-	}
 
 	// TODO (2020-10-06) separate single logics. (onClick(view))
 	override fun onClick(view: View) {
